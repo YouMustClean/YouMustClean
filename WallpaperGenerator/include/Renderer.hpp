2c2c5ea888869966ba9b347389980c9116c63e4e
Joss Lei <josslei@163.com>
2023-01-20 22:10:33 +0800

    Reconstruction

---
+++ WallpaperGenerator/include/Renderer.hpp
@@ -0,0 +1,40 @@
+/**
+ * This file is part of YMC WallpaperGenerator
+ *
+ * Copyright (C) 2022 YouMustClean Team
+ *
+ * YMC WallpaperGenerator is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation, either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * YMC WallpaperGenerator is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with YMC WallpaperGenerator.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+extern "C" {
+#include <log.h>
+}
+
+#include "DataTypes.hpp"
+
+#include <opencv2/opencv.hpp>
+
+#include <string>
+#include <vector>
+
+namespace YMC {
+namespace WallpaperGenerator {
+namespace Renderer {
+
+void putImage(cv::Mat src, Coord offset, cv::Mat dst);
+
+} // Renderer
+} // WallpaperGenerator
+} // YMC
+
