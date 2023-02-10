ccc51742d87386a16963e8541e10355a79c07cdc
Joss Lei <josslei@163.com>
2023-02-10 18:26:48 +0800

    Add configuration parsing and storing module
    1. Change `struct TextConfig` to `class TextConfig`
        - Note: class and struct are basically the same in cpp. It might
        not be a good idea if not intense to make the code compatible in
        C program.
        - Note: And, look at structures that come after in the file... It
        is more neat in this case.
    2. Add namespace YMC::WallpaperGenerator::Configuration
        - This namespace is in charge of TOML configuration sotring and parsing.
    3. Add class CanvasConfig
        - This class stores canvas information like canvas size and canvas
        image path.
        - It feels like I dont need this class because all members are static...
        But, the meaning is to separate data and allow other classes to access
        the `canvas_size`. You will see this later.
    4. Add ImageConfig
        - I stored position as two strings because they could be anything
        in current design of `layout_config.toml`
        - Some examples it could be: "50%" (percentage, in string), "center"
        (linguistic description), "50px" (in pixel, also in string), 50 (integer,
        it can be anything... it would be in pixel if I set the default to pixel)
        - For abstraction (for ease of use), I make `getOffset()` a convenient
        way to get the position.
    5. Add (not implemented) helper function `parseCanvaSizeRelatedNumber()`
        - Most task stated in bullet 3 of #4 will be done by this function.
        - I make this an independent function because I think, later on,
        config like `size` will also need this function.
    
    TODO: Implement parseCanvaSizeRelatedNumber()

--- WallpaperGenerator/include/DataTypes.hpp
+++ WallpaperGenerator/include/DataTypes.hpp
@@ -18,13 +18,15 @@
 #pragma once
 
 #include <vector>
+#include <string>
 
 #include <opencv2/opencv.hpp>
 
 using namespace std;
 using namespace cv;
 
-struct TextConfig {
+class TextConfig {
+public:
 	int height;
 	Point offset;
 	string fontPath, content;
@@ -34,4 +36,59 @@ struct TextConfig {
 	// normally won't be changed
 	int thickness = -1, lineStyle = 8, baseline = 0;
 };
+
+
+/**
+ * YMC::WallpaperGenerator::Configuration
+ * @brief In charge of TOML configuration storing and parsing
+ */
+namespace YMC {
+namespace WallpaperGenerator {
+namespace Configuration {
+
+/**
+ * @brief Parse formatted number (or phrase) in to pixel number
+ * @note Input include percentage (% ending), linguistic description
+ * (e.g. center), and raw pixel number (which can be directly returned).
+ *
+ * @return 1-dimensional coordinate, in pixel
+ */
+int parseCanvaSizeRelatedNumber(std::string number, int canvas_side_length);
+
+class CanvasConfig
+{
+    static int canvas_width;
+    static int canvas_height;
+
+    static std::string path;    ///< path to an image
+
+public:
+    static void setPath(std::string path) { CanvasConfig::path = path; }
+
+    static void autoCanvasSize();
+    static void setCanvasSize(int width, int height)
+    { CanvasConfig::canvas_width = width; CanvasConfig::canvas_height = height; }
+
+    static int getCanvasWidth() { return CanvasConfig::canvas_width; }
+    static int getCanvasHeight() { return CanvasConfig::canvas_height; }
+};  // CanvasConfig
+
+class ImageConfig
+{
+    std::string path;
+
+    // Position
+    std::string position_x;
+    std::string position_y;
+
+public:
+    ImageConfig(std::string path, std::string position_x, std::string position_y);
+
+    std::string getPath() { return this->path; }
+    cv::Point getPosition();
+};  // ImageConfig
+
+} // Configuration
+} // WallpaperGenerator
+} // YMC
 
